15b96c274bbcf0985852e02c387ef88a4a3a5cc8
Wannabe3D <irlitsa10@yandex.ru>
2025-02-26 00:23:11 +0300

    Added test-files and changed tests

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    LOGIN_BUTTON = (By.NAME, "login_submit")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
